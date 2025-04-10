c960593dd7787e9d11e8db40e0ab2cf78c7d78ec
Arijit <arijitbanarjee889@gmail.com>
2025-04-10 12:51:30 +0600

    Cart responsive issue fixed

--- components/pages/cart/CartItem.tsx
+++ components/pages/cart/CartItem.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import React from "react";
-import CartStock from "./CartStock";
 import Button from "@/components/common/Button";
+import Image from "next/image";
+import CartStock from "./CartStock";
 
 const CartItem = () => {
   return (
-    <li className="flex flex-row justify-between items-stretch py-[2rem] gap-5">
+    <li className="flex flex-col sm:flex-row justify-between items-stretch py-[2rem] gap-5">
       <div className="flex flex-col sm:flex-row justify-between gap-5">
         <div className="min-w-[100px]">
           <Image
@@ -13,7 +12,7 @@ const CartItem = () => {
             alt="Product 1"
             width={100}
             height={100}
-            className="aspect-square w-full h-auto"
+            className="aspect-video w-full h-auto"
           />
         </div>
         <div className="w-full">
@@ -35,8 +34,8 @@ const CartItem = () => {
           <CartStock className="justify-start flex mt-3 lg:hidden" />
         </div>
       </div>
-      <div className="text-right">
-        <div className="flex fles-row justify-end items-center gap-4">
+      <div className="text-left sm:text-right">
+        <div className="flex flex-row justify-start sm:justify-end items-center gap-4">
           <CartStock className="justify-end hidden lg:flex" />
           <h2 className="text-[#111] uppercase font-bold leading-[1.25] tracking-[.0625em] text-[1.5rem]">
             $52,000.00
